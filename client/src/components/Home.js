e00e35b6785502bec8b5f06a723f141fb66f6b52
Andrew Locke <alocke12992@gmail.com>
2018-04-25 23:55:36 -0600

    adds slap counter

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { Header, Grid, Image} from 'semantic-ui-react';
+import image from '../assets/ANDREW.png'
+
+const kaPow = ["Smack", "Kapow", "WHAM", "FLAP"]
 
 class Home extends Component {
-  state = {x: 250, y: 300}
+  state = {x: 250, y: 300, hits: 0, message: ""}
 
   componentDidMount() {
     window.addEventListener('mousemove', this.handleMouseMove)
@@ -11,10 +14,35 @@ class Home extends Component {
   handleMouseMove = ({pageX: x, pageY: y}) => {
     this.setState({x, y})
   }
-  
+
+  hit = () => {
+    const kaPow = ["Smack", "Kapow", "WHAM", "FLAP"]
+    let rand = kaPow[Math.floor(Math.random() * kaPow.length)];
+    debugger
+    const {hits} = this.state
+    this.setState({ hits: hits + 1, message: rand})
+    this.fadeSlap() 
+  }
+
+  fadeSlap = () => {
+    setTimeout(() => {
+      this.setState({message: ""})
+    }, 500);
+  }
+
   render() {
+    const {hits} = this.state
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <div>
+        <Header as='h1' textAlign='center'>Hits: <br />{hits}</Header>
+        {this.state.message === "" ? 
+        null
+        :
+          <Header as="h2" textAlign='center'>{this.state.message}</Header>
+        }
+        <Image onMouseOver={this.hit} src={image} />
+
+      </div>
     );
   }
 }
