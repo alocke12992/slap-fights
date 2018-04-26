53a5542a67dcbb87312d4586a707df7bc3c72ab1
Andrew Locke <alocke12992@gmail.com>
2018-04-25 23:17:39 -0600

    Initial Commit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Grid, Image} from 'semantic-ui-react';
 
 class Home extends Component {
+  state = {x: 250, y: 300}
+
+  componentDidMount() {
+    window.addEventListener('mousemove', this.handleMouseMove)
+  } 
+
+  handleMouseMove = ({pageX: x, pageY: y}) => {
+    this.setState({x, y})
+  }
+  
   render() {
     return (
       <Header as='h1' textAlign='center'>Home Component</Header>
